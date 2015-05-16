1be8c52db0f82f66b994cea70b1569a89019becc
Mirhat <babic.mirhat@gmail.com>
2015-05-16 18:50:19 +0200

    init opet

--- init.sql
+++ init.sql
@@ -20,7 +20,8 @@ SET time_zone = "+00:00";
 -- Database: `tim3`
 --
 
-CREATE DATABASE tim3;
+CREATE DATABASE `tim3`;
+USE DATABASE `tim3`;
 
 -- --------------------------------------------------------
 
