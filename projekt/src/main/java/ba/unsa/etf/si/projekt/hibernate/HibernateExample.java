d443a64968205f1eb380cbfba5e7bc9e55556aab
Mirhat <babic.mirhat@gmail.com>
2015-05-16 13:24:43 +0200

    pomjeren hibernate.cfg.xml

---
+++ projekt/src/main/java/ba/unsa/etf/si/projekt/hibernate/HibernateExample.java
@@ -0,0 +1,35 @@
+package ba.unsa.etf.si.projekt.hibernate;
+import org.hibernate.Transaction;
+import org.hibernate.Session;
+
+import ba.unsa.etf.si.projekt.entiteti.Autobus;
+
+public class HibernateExample {
+	public static void main(String[] args) {
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		dodajAutobus(session);
+		nadjiAutobus(session, 12);
+		session.close();
+	}
+	private static void dodajAutobus(Session session) {
+		Transaction t = session.beginTransaction();
+		
+		Autobus a = new Autobus();
+		a.setKapacitet(39);
+		a.setModel("Mercedes");
+		a.setRegistracija("aaa-aa-aaa");
+		
+		Long id = (Long) session.save(a);
+		t.commit();
+	}
+	private static void nadjiAutobus(Session session, long id) {
+		Transaction t = session.beginTransaction();
+		Autobus s = (Autobus) session.get(Autobus.class, id);
+		if (s==null) {
+			System.out.println("Nema autobusa sa tim IDom u bazi");
+		} else {
+			System.out.println("Autobus: "+s.getKapacitet()+" "+s.getModel()+" "+s.getRegistracija());
+		}
+		t.commit();
+	}
+}
