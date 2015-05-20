a37f8c514d9d0ff5da95132ad02ce79c08f85d1a
Larisa Besic <lbesic1@etf.unsa.ba>
2015-05-20 22:28:09 +0200

    validacija (prazno polje, JMBG)

--- projekt/src/main/java/ba/unsa/etf/si/projekt/dodatno/Validacija.java
+++ projekt/src/main/java/ba/unsa/etf/si/projekt/dodatno/Validacija.java
@@ -1,5 +1,44 @@
 package ba.unsa.etf.si.projekt.dodatno;
 
-public class Validacija {
-
+public class Validacija 
+{
+    public static boolean praznoPolje(String vrijednost)
+	{
+    	if (vrijednost == null || vrijednost.trim().equals( "" ))
+    	{
+    		return true;
+    	}
+    	else
+    	{
+    		return false;
+    	}
+	}
+    
+    public static boolean validirajJMBG(String vrijednost)
+	{
+    	if (vrijednost != null && vrijednost.trim().equals( "" ) && jeInt(vrijednost) == true && vrijednost.length() == 13)
+    	{
+    		return true;
+    	}
+    	else
+    	{
+    		return false;
+    	}
+	}
+    
+    public static boolean jeInt(String vrijednost)
+    {
+        try 
+        { 
+            Integer.parseInt(vrijednost); 
+        } catch(NumberFormatException e) 
+        { 
+            return false; 
+        } catch(NullPointerException e) 
+        {
+            return false;
+        }
+        return true;
+    }
+    
 }
