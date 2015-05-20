2d0f9bac1f2547010d6a87a170de8dcb01b6dc9a
Saudin Ademovic <saudin.12@hotmail.com>
2015-05-20 23:23:16 +0200

    Validacija(pasword,korisnicko ime, radna pozicija

--- projekt/src/main/java/ba/unsa/etf/si/projekt/dodatno/Validacija.java
+++ projekt/src/main/java/ba/unsa/etf/si/projekt/dodatno/Validacija.java
@@ -1,5 +1,7 @@
 package ba.unsa.etf.si.projekt.dodatno;
 
+import java.util.List;
+
 public class Validacija 
 {
     public static boolean praznoPolje(String vrijednost)
@@ -25,6 +27,8 @@ public class Validacija
     		return false;
     	}
 	}
+
+   
     
     public static boolean jeInt(String vrijednost)
     {
@@ -40,5 +44,59 @@ public class Validacija
         }
         return true;
     }
+    
+    public static boolean sadrziCifru(String s){  
+        boolean sadrzi = false;
+
+        if(s != null && ! s.isEmpty()){
+            for(char c : s.toCharArray()){
+                if(sadrzi = Character.isDigit(c)){
+                	
+                    break;
+                }
+            }
+        }
+
+        return sadrzi;
+    }
+    public static boolean alphaNumerickaVrijednost(String s){  
+        boolean jeste = true;
+
+        if(s != null && ! s.isEmpty()){
+            for(char c : s.toCharArray()){
+                if(!(Character.isDigit(c) || Character.isLetter(c) )){
+                	jeste=false;
+                	break;
+                	}
+            }
+        }
+
+        return jeste;
+    }
+    
+    public static boolean validirajPass(String vrijednost){
+    	if (vrijednost != null && vrijednost.trim().equals( "" ) && vrijednost.length() >= 8 && sadrziCifru(vrijednost) && !alphaNumerickaVrijednost(vrijednost))
+    	{
+    		return true;
+    	}
+    	else
+    	{
+    		return false;
+    	}
+    	
+    }
+    
+    public static boolean korisnickoIme(List<String> imena, String ime)
+    {
+       if(imena.contains(ime)) return false;
+       else return true;
+    }
+    
+    public static boolean radnaPozicija(String rpozicija)
+    {
+       if(rpozicija=="Menadzer"|| rpozicija=="Administrator"|| rpozicija=="SalterskiRadnik") return true;
+       else return false;
+    }
+    
     
 }
