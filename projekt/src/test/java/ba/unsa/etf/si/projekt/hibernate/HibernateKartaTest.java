55fceed5ff59f265100a6b3e2e4016a7fbc3e0a8
sumejabotulja <sbotulja1@etf.unsa.ba>
2015-05-22 19:56:50 +0200

    proba test

---
+++ projekt/src/test/java/ba/unsa/etf/si/projekt/hibernate/HibernateKartaTest.java
@@ -0,0 +1,34 @@
+package ba.unsa.etf.si.projekt.hibernate;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class HibernateKartaTest {
+
+	@Test
+	public void testMain() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDodajKartu() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSveKarte() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIzvjestajOProdanimKartama() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testHibernateKarta() {
+		fail("Not yet implemented");
+	}
+
+}
