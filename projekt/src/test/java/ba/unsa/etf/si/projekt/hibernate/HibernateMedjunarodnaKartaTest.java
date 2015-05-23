9a881dc1d0f3152ff80943a91079c24e02fb3898
Dina <dinaahmic@hotmail.com>
2015-05-23 22:34:22 +0200

    medjunarodna karta test dodavanja

--- projekt/src/test/java/ba/unsa/etf/si/projekt/hibernate/HibernateMedjunarodnaKartaTest.java
+++ projekt/src/test/java/ba/unsa/etf/si/projekt/hibernate/HibernateMedjunarodnaKartaTest.java
@@ -7,8 +7,12 @@ import org.hibernate.Session;
 import org.junit.Before;
 import org.junit.Test;
 
+import ba.unsa.etf.si.projekt.entiteti.Autobus;
 import ba.unsa.etf.si.projekt.entiteti.AutobuskaLinija;
+import ba.unsa.etf.si.projekt.entiteti.Karta;
+import ba.unsa.etf.si.projekt.entiteti.Radnik;
 import ba.unsa.etf.si.projekt.entiteti.TipKarte;
+import ba.unsa.etf.si.projekt.entiteti.TipRadnogMjesta;
 
 public class HibernateMedjunarodnaKartaTest {
 	
@@ -23,11 +27,41 @@ public class HibernateMedjunarodnaKartaTest {
 		tip = TipKarte.jednosmjerna;
 	}
 	
-/*	@Test
+	@Test
 	public void testDodajKartu() {
-		
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		hmk = new HibernateMedjunarodnaKarta();
+		HibernateAutobus ha = new HibernateAutobus();
+		ha.dodajAutobus(session, 50, "A23-M-000", "Testmodel");
+		HibernateRadnik hr = new HibernateRadnik();
+		TipRadnogMjesta vozac = TipRadnogMjesta.Vozac;
+		hr.dodajRadnika(session, "DinaTestTest", "Aaa", "1223493827100", vozac);
+		Autobus a = ha.nadjiAutobus(session, "A23-M-111");  
+		Radnik r = hr.nadjiRadnika(session, "1223493827144");
+		HibernateAutibuskaLinija hal = new HibernateAutibuskaLinija();
+		hal.dodajAutobuskuLiniju(session,"SarajevoTest","ZenicaTest",a,r,2015,4,17,7,7,5,100,100,4,20,40,true);
+		linija = hal.nadjiAutobuskuLiniju(session, 4); //linija 4
+		tip = TipKarte.jednosmjerna;
+		//prije dodavanja
+		Query q = session.createQuery("SELECT COUNT(*) FROM MedjunarodnaKarta");
+		Long count = (Long)q.uniqueResult();
+		int prije = count.intValue();
+		hmk.dodajKartu(session, linija, 2015, 4, 17, 7, 7, tip, 20.00, "Dinatesttest", "dk");
+		//poslije dodavanja
+		HibernateKarta hk = new HibernateKarta();
+		Query q2 = session.createQuery("SELECT COUNT(*) FROM MedjunarodnaKarta");
+		Long count2 = (Long)q.uniqueResult();
+		int poslije = count2.intValue();
+		assertEquals(poslije, prije+1);
+		Query q3 = session.createQuery("DELETE FROM MedjunarodnaKarta WHERE ime='Dinatesttest'");
+		q3.executeUpdate();
+		//hk.brisanjeKarte(session, linija.getId());
+		hal.brisiAutobuskuLiniju(session, 4);
+		ha.brisanjeAutobusa(session, "A23-M-000");
+		hr.brisiRadnika(session, "1223493827100");
+		session.close();
 	}
-*/
+
 	@Test
 	public void testSveMedjunarodneKarte() {
 		Session session = HibernateUtil.getSessionFactory().openSession();
