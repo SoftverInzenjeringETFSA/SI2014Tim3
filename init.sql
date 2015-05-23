e3616387b67c8ee6f4b6d0105297d44de43442b5
Mirhat <babic.mirhat@gmail.com>
2015-05-23 11:14:33 +0200

    novi init.sql

--- init.sql
+++ init.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: May 18, 2015 at 10:59 PM
+-- Generation Time: May 23, 2015 at 11:03 AM
 -- Server version: 5.6.17
 -- PHP Version: 5.5.12
 
@@ -25,20 +25,10 @@ USE `tim3`;
 
 -- --------------------------------------------------------
 
-CREATE USER 'EtfSI2014' IDENTIFIED BY '2014SIEtf';
+CREATE USER 'EtfSI2014'@'localhost' IDENTIFIED BY '2014SIEtf';
 
 GRANT ALL ON `Tim3`.* TO 'EtfSI2014';
 
---
--- Table structure for table `administrator`
---
-
-CREATE TABLE IF NOT EXISTS `administrator` (
-  `ID` bigint(20) NOT NULL,
-  PRIMARY KEY (`ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
 
 --
 -- Table structure for table `autobus`
@@ -50,14 +40,7 @@ CREATE TABLE IF NOT EXISTS `autobus` (
   `KAPACITET` int(11) DEFAULT NULL,
   `REGISTRACIJA` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`ID`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
-
---
--- Dumping data for table `autobus`
---
-
-INSERT INTO `autobus` (`ID`, `MODEL`, `KAPACITET`, `REGISTRACIJA`) VALUES
-(2, 'BMW', 32, 'abc-d-efg');
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -74,8 +57,8 @@ CREATE TABLE IF NOT EXISTS `autobuskalinija` (
   `DATUMPOLASKA_DAN` int(11) DEFAULT NULL,
   `DATUMPOLASKA_MJESEC` int(11) DEFAULT NULL,
   `DATUMPOLASKA_GODINA` int(11) DEFAULT NULL,
-  `VRIJEMEPOLASKA_SATI` double DEFAULT NULL,
-  `VRIJEMEPOLASKA_MINUTE` double DEFAULT NULL,
+  `VRIJEMEPOLASKA_SATI` int(11) DEFAULT NULL,
+  `VRIJEMEPOLASKA_MINUTE` int(11) DEFAULT NULL,
   `PERON` int(11) DEFAULT NULL,
   `DISTANCA` double DEFAULT NULL,
   `TRAJANJE` double DEFAULT NULL,
@@ -83,17 +66,11 @@ CREATE TABLE IF NOT EXISTS `autobuskalinija` (
   `CIJENAJEDNOSMJERNA` double DEFAULT NULL,
   `CIJENADVOSMJERNA` double DEFAULT NULL,
   `MEDJUNARODNA` bit(1) DEFAULT NULL,
+  `ZAUZETO` int(11) DEFAULT NULL,
   PRIMARY KEY (`ID`),
   KEY `FK_j38341cnju2t1qvypfrp8sm3c` (`AUTOBUS`),
   KEY `FK_lp207a8l8k8sf1ttvjnl63xuy` (`VOZAC`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
-
---
--- Dumping data for table `autobuskalinija`
---
-
-INSERT INTO `autobuskalinija` (`ID`, `POLAZISTE`, `ODREDISTE`, `AUTOBUS`, `VOZAC`, `DATUMPOLASKA_DAN`, `DATUMPOLASKA_MJESEC`, `DATUMPOLASKA_GODINA`, `VRIJEMEPOLASKA_SATI`, `VRIJEMEPOLASKA_MINUTE`, `PERON`, `DISTANCA`, `TRAJANJE`, `BROJLINIJE`, `CIJENAJEDNOSMJERNA`, `CIJENADVOSMJERNA`, `MEDJUNARODNA`) VALUES
-(1, 'Zenica', 'Tuzla', 2, 1, 0, 5, 2015, 2, 2, 2, 100, 24, 2, 20, 40, b'0');
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -103,15 +80,16 @@ INSERT INTO `autobuskalinija` (`ID`, `POLAZISTE`, `ODREDISTE`, `AUTOBUS`, `VOZAC
 
 CREATE TABLE IF NOT EXISTS `karta` (
   `ID` bigint(20) NOT NULL AUTO_INCREMENT,
-  `ODREDISTE` varchar(255) DEFAULT NULL,
-  `VRIJEMEPOLASKA_SATI` double DEFAULT NULL,
-  `VRIJEMEPOLASKA_MINUTE` double DEFAULT NULL,
+  `linija` bigint(20) DEFAULT NULL,
+  `VRIJEMEKupovine_SATI` int(11) DEFAULT NULL,
+  `VRIJEMEkupovine_MINUTE` int(11) DEFAULT NULL,
   `TIPKARTE` varchar(255) DEFAULT NULL,
-  `DATUMPOLASKA_DAN` int(11) DEFAULT NULL,
-  `DATUMPOLASKA_MJESEC` int(11) DEFAULT NULL,
-  `DATUMPOLASKA_GODINA` int(11) DEFAULT NULL,
+  `DATUMkupovine_DAN` int(11) DEFAULT NULL,
+  `DATUMkupovine_MJESEC` int(11) DEFAULT NULL,
+  `DATUMkupovine_GODINA` int(11) DEFAULT NULL,
   `CIJENA` double DEFAULT NULL,
-  PRIMARY KEY (`ID`)
+  PRIMARY KEY (`ID`),
+  KEY `FK_nb4vtpx7e9tr2a9mivsjue6mg` (`linija`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
@@ -124,11 +102,10 @@ CREATE TABLE IF NOT EXISTS `korisnickiracun` (
   `ID` bigint(20) NOT NULL AUTO_INCREMENT,
   `KORISNICKOIME` varchar(255) DEFAULT NULL,
   `SIFRA` varchar(255) DEFAULT NULL,
-  `IME` varchar(255) DEFAULT NULL,
-  `PREZIME` varchar(255) DEFAULT NULL,
-  `JMBG` varchar(255) DEFAULT NULL,
+  `radnik` bigint(20) DEFAULT NULL,
   `TIPKORISNICKOGRACUNA` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`ID`)
+  PRIMARY KEY (`ID`),
+  KEY `FK_qppepa8vt2jotww7d6fhv901f` (`radnik`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
@@ -175,14 +152,7 @@ CREATE TABLE IF NOT EXISTS `radnik` (
   `JMBG` varchar(255) DEFAULT NULL,
   `TIPRADNOGMJESTA` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`ID`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
-
---
--- Dumping data for table `radnik`
---
-
-INSERT INTO `radnik` (`ID`, `IME`, `PREZIME`, `JMBG`, `TIPRADNOGMJESTA`) VALUES
-(1, 'Sumeja', 'Botulja', '2412993178512', 'Vozac');
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -191,37 +161,43 @@ INSERT INTO `radnik` (`ID`, `IME`, `PREZIME`, `JMBG`, `TIPRADNOGMJESTA`) VALUES
 --
 
 CREATE TABLE IF NOT EXISTS `rezervacija` (
-  `ID` bigint(20) NOT NULL,
-  PRIMARY KEY (`ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `salterskiradnik`
---
-
-CREATE TABLE IF NOT EXISTS `salterskiradnik` (
-  `ID` bigint(20) NOT NULL,
-  PRIMARY KEY (`ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `ID` bigint(20) NOT NULL AUTO_INCREMENT,
+  `LINIJA` bigint(20) DEFAULT NULL,
+  `VRIJEMEPOLASKA_SATI` int(11) DEFAULT NULL,
+  `VRIJEMEPOLASKA_MINUTE` int(11) DEFAULT NULL,
+  `TIPKARTE` varchar(255) DEFAULT NULL,
+  `DATUMPOLASKA_DAN` int(11) DEFAULT NULL,
+  `DATUMPOLASKA_MJESEC` int(11) DEFAULT NULL,
+  `DATUMPOLASKA_GODINA` int(11) DEFAULT NULL,
+  `CIJENA` double DEFAULT NULL,
+  `IME` varchar(255) DEFAULT NULL,
+  `PREZIME` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`ID`),
+  KEY `FK_tr1si9pqlsit85x4n5gw6i2ng` (`LINIJA`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 --
 -- Constraints for dumped tables
 --
 
 --
--- Constraints for table `administrator`
---
-ALTER TABLE `administrator`
-  ADD CONSTRAINT `FK_plxxsytu0rbjtg36p6v8ty0tu` FOREIGN KEY (`ID`) REFERENCES `radnik` (`ID`);
-
---
 -- Constraints for table `autobuskalinija`
 --
 ALTER TABLE `autobuskalinija`
-  ADD CONSTRAINT `FK_j38341cnju2t1qvypfrp8sm3c` FOREIGN KEY (`AUTOBUS`) REFERENCES `autobus` (`ID`),
-  ADD CONSTRAINT `FK_lp207a8l8k8sf1ttvjnl63xuy` FOREIGN KEY (`VOZAC`) REFERENCES `radnik` (`ID`);
+  ADD CONSTRAINT `FK_lp207a8l8k8sf1ttvjnl63xuy` FOREIGN KEY (`VOZAC`) REFERENCES `radnik` (`ID`),
+  ADD CONSTRAINT `FK_j38341cnju2t1qvypfrp8sm3c` FOREIGN KEY (`AUTOBUS`) REFERENCES `autobus` (`ID`);
+
+--
+-- Constraints for table `karta`
+--
+ALTER TABLE `karta`
+  ADD CONSTRAINT `FK_nb4vtpx7e9tr2a9mivsjue6mg` FOREIGN KEY (`linija`) REFERENCES `autobuskalinija` (`ID`);
+
+--
+-- Constraints for table `korisnickiracun`
+--
+ALTER TABLE `korisnickiracun`
+  ADD CONSTRAINT `FK_qppepa8vt2jotww7d6fhv901f` FOREIGN KEY (`radnik`) REFERENCES `radnik` (`ID`);
 
 --
 -- Constraints for table `medjunarodnakarta`
@@ -239,13 +215,7 @@ ALTER TABLE `nalog`
 -- Constraints for table `rezervacija`
 --
 ALTER TABLE `rezervacija`
-  ADD CONSTRAINT `FK_luqqtt9fdbew8mejouqw27qht` FOREIGN KEY (`ID`) REFERENCES `karta` (`ID`);
-
---
--- Constraints for table `salterskiradnik`
---
-ALTER TABLE `salterskiradnik`
-  ADD CONSTRAINT `FK_fx9vqcls1hmj9rpv8ua4f9igv` FOREIGN KEY (`ID`) REFERENCES `radnik` (`ID`);
+  ADD CONSTRAINT `FK_tr1si9pqlsit85x4n5gw6i2ng` FOREIGN KEY (`LINIJA`) REFERENCES `autobuskalinija` (`ID`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
